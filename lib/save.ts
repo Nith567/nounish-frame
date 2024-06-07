df7476af13a47c19d7a78d3f62d79ab21aee726d
Nithin <nithinkatkam504106@gmail.com>
2024-06-07 13:49:37 +0530

    added functions

--- lib/save.ts
+++ lib/save.ts
@@ -19,3 +19,28 @@ export async function saved(fid: number) {
     const id = fid.toString();
     await redis.hdel("played", id);
 }
+
+//saves fid with color
+export async function saveColor(fid: number, color: string) {
+  const id = fid.toString();
+  await redis.hset("colors", id, color);
+}
+
+//tracks fid with color 
+export async function checkColor(fid: number): Promise<string | boolean> {
+  const id = fid.toString();
+  const color = await redis.hget("colors", id);
+  return color !== null ? color : false;
+}
+
+//deltes fid with color use this function while testing 
+export async function delColor(fid: number): Promise<boolean> {
+  const id = fid.toString();
+  const colorExists = await checkColor(fid); 
+  if (typeof colorExists === 'string') {
+    await redis.hdel("colors", id);
+    return true;
+  } else {
+    return false;
+  }
+}
